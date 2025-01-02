6f13d7558ef8f572d13f45546c04f0db7cd5122c
galaxy4276 <deveungi@gmail.com>
2025-01-02 22:15:30 +0900

    feat: add query configuration

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/shared/client";
 
 type Props = {
   children: ReactNode;
 }
 
 export default function App({ children }: Props) {
-  return children;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
 }
