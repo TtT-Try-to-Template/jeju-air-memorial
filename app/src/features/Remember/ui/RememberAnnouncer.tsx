92b240e3600dd0ab7ba997da5d15a768dc71a05e
galaxy4276 <deveungi@gmail.com>
2025-01-02 22:15:42 +0900

    feat: add random nickname generator

--- app/src/features/Remember/ui/RememberAnnouncer.tsx
+++ app/src/features/Remember/ui/RememberAnnouncer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Typo } from "@/shared/ui";
+import { useMemorialTotalCountQuery } from "@/features/Memorialize";
 
 const RememberAnnouncer: React.FC = () => {
+  const { data } = useMemorialTotalCountQuery();
+
   return (
     <div className="flex gap-x-2 py-[32px]">
       <Typo>함께 기억해주신 분들</Typo>
-      <Typo className="font-bold">{0}명</Typo>
+      <Typo className="font-bold">{data.count} 명</Typo>
     </div>
   );
 }
