25fe8e1d16df2e928a842c21ac1e7e69f4020f23
galaxy4276 <deveungi@gmail.com>
2025-01-04 01:12:50 +0900

    feat: add google form link

--- app/src/features/Memorialize/ui/BottomNav.tsx
+++ app/src/features/Memorialize/ui/BottomNav.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Button } from "@/shared/ui";
+import { Button, Typo } from "@/shared/ui";
 import { cn } from "@/lib/utils";
 import { useNavigate } from "react-router";
 import { urlPaths } from "@/features/Router";
+
+const formLink = 'https://docs.google.com/forms/d/e/1FAIpQLSfv9I6DWbfp35nF5UnHrA3UiOl-wxshc-G1LlfSOTEw4iearA/viewform?usp=sharing';
+const openGoogleForm = () => window.open(formLink, '_blank');
 
 const BottomNav: React.FC = () => {
   const navigate = useNavigate();
@@ -21,6 +24,17 @@ const BottomNav: React.FC = () => {
       >
         마음 전달하러 가기
       </Button>
+
+      <div className="flex w-full justify-center">
+        <Typo
+          size="lg"
+          className="pt-3 underline underline-offset-1 cursor-pointer"
+          onClick={openGoogleForm}
+        >
+          문의하기
+        </Typo>
+      </div>
+
     </section>
   );
 }
